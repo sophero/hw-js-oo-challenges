a599687c7a221998306e4ad37d1feed3778d720f
Sophia Di <backwardsxmarathon@gmail.com>
2017-04-19 17:35:08 -0400

    Adds methods to constructor functions, instantiates objects, performs example uses of methods.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Question One //
+console.log("\n Question One");
 
 function Multiplier() {
 	this.currentVal = 1;
@@ -21,6 +22,7 @@ console.log(multiplier.getCurrentValue());
 
 
 // Question Two //
+console.log("\n Question Two");
 
 var seneca = new Photo("seneca-1.jpg", "Philadelphia, PA");
 var portHills = new Photo("Port_Hills_on_fire_15_feb.jpg", "Christchurch, New Zealand");
@@ -55,6 +57,28 @@ function Album(photoArray) {
 
 
 // Question Three //
+console.log("\n Question Three");
+
+var john = new Person("John", "Smith", 52);
+var sarah = new Student("Sarah", "Strawbridge", 11);
+var briony = new Student("Briony", "Lightbody", 13);
+var craig = new Teacher("Craig", "Robinson", 46);
+var gillian = new Teacher("Gillian", "Trotter", 64);
+
+var south = new School("South Intermediate", [sarah], [gillian]);
+
+console.log(john.fullName());
+south.addStudent(briony);
+south.addTeacher(craig);
+gillian.assignGrade(sarah, 96);
+gillian.assignHomework(sarah);
+sarah.completeHomework();
+sarah.giveApple(gillian);
+craig.giveDetention(briony, 3);
+briony.attendDetention();
+south.listStudents();
+south.listTeachers();
+
 
 function Person(fname, lname, age) {
 	this.fname = fname;
@@ -66,19 +90,90 @@ function Person(fname, lname, age) {
 }
 
 function Student(fname, lname, age, school) {
-	this.isStudying = true;
 	Person.apply(this, arguments);
+	this.isStudying = true;
+	this.numDetentions = 0;
+	this.currentGrade;
+	this.hasHomework;
 	this.school = school;
+	this.attendDetention = function() {
+		this.numDetentions -= 1;
+		console.log(this.fullName() + " attended detention and now has " + this.numDetentions + " detentions remaining.");
+	}
+	this.completeHomework = function() {
+		this.hasHomework = false;
+		console.log(this.fullName() + " has completed their homework.");
+	}
+	this.giveApple = function(teacher) {
+		teacher.hasApple = true;
+		console.log(this.fullName() + " gave " + teacher.fullName() + " an apple.");
+	}
 }
 
 function Teacher(fname, lname, age, school) {
-	this.isTeaching = true;
 	Person.apply(this, arguments);
+	this.isTeaching = true;
 	this.school = school;
+	this.hasApple;
+	this.assignGrade = function(student, grade) {
+		student.currentGrade = grade;
+		console.log(this.fullName() + " assigned a grade of " + grade + " to " + student.fullName() + ".");
+	}
+	this.giveDetention = function(student, numDetentions) {
+		student.numDetentions += numDetentions;
+		console.log(this.fullName() + " gave " + student.fullName() + " " + numDetentions + " detentions.");
+	}
+	this.assignHomework = function(student) {
+		student.hasHomework = true;
+		console.log(this.fullName() + " assigned homework to " + student.fullName() + ".");
+	}
 }
 
 function School(name, studentArray, teacherArray) {
 	this.name = name;
 	this.students = studentArray;
 	this.teachers = teacherArray;
+
+	for (var k = 0; k < studentArray.length; k++) {
+		studentArray[k].school = this.name;
+	}
+	for (var k = 0; k < teacherArray.length; k++) {
+		teacherArray[k].school = this.name;
+	}
+	
+	
+	this.listStudents = function() {
+		for (var k = 0; k < this.students.length; k++) {
+			console.log(this.students[k]);
+		}
+	}
+	this.listTeachers = function() {
+		for (var k = 0; k < this.teachers.length; k++) {
+			console.log(this.teachers[k]);
+		}
+	}
+	this.addStudent = function(student) {
+		this.students.push(student);
+		student.school = this.name;
+		console.log(student.fullName() + " added to school " + this.name + ".");
+	}
+	this.addTeacher = function(teacher) {
+		this.teachers.push(teacher);
+		teacher.school = this.name;
+		console.log(teacher.fullName() + " added to school " + this.name + ".");
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
